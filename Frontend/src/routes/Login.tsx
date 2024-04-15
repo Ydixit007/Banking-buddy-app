1161b2c568c66c1b152664c4823aa5ac47d585ca
Yash Dixit <ydixit007@gmail.com>
2024-04-15 21:14:41 +0530

    changed the h1

--- Frontend/src/routes/Login.tsx
+++ Frontend/src/routes/Login.tsx
@@ -4,8 +4,8 @@ import React from "react";
 const Login = () => {
   return (
     <div className="h-screen w-full bg-background flex justify-center items-center flex-col gap-4">
-      <h1 className="text-primary text-xl text-center">
-        Welcome to <br /><span className="text-bankPrimary text-2xl">Banking-Buddy</span>
+      <h1 className="text-primary text-sm text-center mb-6">
+        Welcome back! <br /><span className="text-bankPrimary text-2xl">Banking-Buddy</span>
       </h1>
       <LoginForm />
     </div>
